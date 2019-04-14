705a00720899d5dd4e4223173bb0cc0ebb350489
seungsukim <seungsu0407@gmail.com>
2019-04-15 01:37:57 +0900

    Updates

---
+++ precache-manifest.0e914355ace2faf643da9243583d1ce9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70ef67903b73c78e752e",
+    "url": "/MovieApp/static/css/main.d0976512.chunk.css"
+  },
+  {
+    "revision": "70ef67903b73c78e752e",
+    "url": "/MovieApp/static/js/main.b3cd32d5.chunk.js"
+  },
+  {
+    "revision": "dedbc3b85a4301ff89be",
+    "url": "/MovieApp/static/js/runtime~main.9917d67f.js"
+  },
+  {
+    "revision": "6303b1dc6d95fb6846a7",
+    "url": "/MovieApp/static/js/2.9dee5afb.chunk.js"
+  },
+  {
+    "revision": "bd12895818c70d1d2b75786d2f5516e8",
+    "url": "/MovieApp/index.html"
+  }
+];
