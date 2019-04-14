4e74fedf27a11d335a793b3b243de44106aaca29
seungsukim <seungsu0407@gmail.com>
2019-04-15 00:51:54 +0900

    #7-1 Updating Movie Component

--- src/App.js
+++ src/App.js
@@ -11,9 +11,14 @@ class App extends Component {
   }
 
   _renderMovies = () => {
-    console.log(this.state.movies)
     const movies = this.state.movies.map(movie => {
-      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
+      return <Movie
+        title={movie.title_english}
+        poster={movie.medium_cover_image}
+        key={movie.id}
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+      />
     })
     console.log(movies)
     return movies
